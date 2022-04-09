dc9b2859c23c1dbda1cd55a5fb6f4d5a26c31a45
amsewr <100431767+amsewr@users.noreply.github.com>
2022-04-09 16:13:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ if uploaded_file:
     
 model = st.sidebar.radio('Quelle méthode de prédiction voulez-vous utiliser ?',("Réseaux de neurones", "SVM"))
 if model =="Réseaux de neurones":
-    option = st.sidebar.selectbox('Quel modèle voulez-vous utiliser ?',("Choisissez un modèle", MODEL_PATH_FAMILY, MODEL_PATH_MANUFACTURER, MODEL_PATH_VARIANT))
+    option = st.sidebar.selectbox('Que voulez-vous identifier ?',("Choisissez un modèle", MODEL_PATH_FAMILY, MODEL_PATH_MANUFACTURER, MODEL_PATH_VARIANT))
 
     if option != "Choisissez un modèle": 
         model = load_model(option)
